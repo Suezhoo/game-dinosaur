bd2579d00aa3eba3d56a73d41feaf131488a08da
Suezhoo <suezhoo@outlook.com>
2022-01-17 20:14:31 +0100

    Dino is jumping

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 "use strict";
 
+//dino character
+const char = document.getElementById("char");
+
 window.onload = function () {
     let gameRunning = false;
-    let char = document.querySelector("[data-char]");
-    document.addEventListener("keyup", (e) => {
+    document.addEventListener("keydown", (e) => {
         if (e.code === "Space" && gameRunning == false) {
-            console.log("Starting game");
-            document.querySelector("[data-start-text]").classList.toggle("hide");
-            document.querySelector("[data-game]").classList.toggle("game-fullscreen");
-            char.classList.toggle("char-fullscreen");
+            gameRunning = true;
+            console.log("Gamerunning:", gameRunning);
+            render();
+        } else if (e.code === "Space" && gameRunning == true) {
+            jump();
         }
     });
 };
+
+function render() {
+    console.log(char);
+    console.log("Rendering game");
+    document.querySelector("[data-start-text]").classList.toggle("hide");
+    document.querySelector("[data-game]").classList.toggle("game-fullscreen");
+    document.querySelector(".scores").classList.toggle("hide");
+    char.classList.toggle("char-fullscreen");
+}
+
+function jump() {
+    if (!char.classList.contains("jump")) {
+        char.classList.add("jump");
+        setTimeout(function () {
+            char.classList.remove("jump");
+        }, 500);
+    }
+}
