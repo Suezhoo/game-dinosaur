f50ad30b71d90ec9c5603017dce4bb9c1c59a23d
Suezhoo <suezhoo@outlook.com>
2022-01-18 22:39:16 +0100

    Implemented scoring system

--- script.js
+++ script.js
@@ -16,20 +16,19 @@ window.onload = function () {
     document.addEventListener("keydown", (e) => {
         if (e.code === "Space" && gameRunning == false) {
             gameRunning = true;
-            console.log("Gamerunning:", gameRunning);
             render();
+            runGame();
         } else if (e.code === "Space" && gameRunning == true) {
             dino.state = "jumping";
             jump();
         }
-        if (gameRunning == true) runAnimation();
     });
 };
 
 function render() {
     // Add obstacles
     document.querySelector("[data-obstacles]").classList.remove("hide");
-    addObstacle();
+    // addObstacle();
     // Change game to fullscreen
     document.querySelector("[data-game]").classList.add("game-fullscreen");
     // Make dino bigger
@@ -42,17 +41,14 @@ function render() {
 let counter = 0;
 let i = 0;
 function runAnimation() {
-    const fps = 60;
     counter += 1;
-    // console.log(`%cFPS Count: %c${counter % fps}`, "font-weight: bold", "color: limegreen");
-    // console.log((counter % fps) % 11);
-    // console.log(counter);
     const dinoImages = [
         '<img class="char char-fullscreen" id="char" src="./assets/img/dino.png" alt="dino" data-char />',
         '<img class="char char-fullscreen" id="char" src="./assets/img/dino1.png" alt="dino" data-char />',
         '<img class="char char-fullscreen" id="char" src="./assets/img/dino2.png" alt="dino" data-char />',
     ];
 
+    // Alternate dino pictures every 5 frames
     if (counter % 5 === 0) {
         if (i > 2) {
             i = 0;
@@ -61,9 +57,44 @@ function runAnimation() {
         i++;
     }
 
+    // Freeze image if jumping
     if (dino.state == "jumping") char.innerHTML = dinoImages[0];
 
-    if (dino.isAlive && dino.state == "running")
+    // Callback to keep animation running
+    runGame();
+}
+
+let score = 0;
+function runGame() {
+    const fps = 60;
+    if (counter % 20 === 0) {
+        score += 1;
+    }
+
+    // Scoring
+
+    const _score = document.querySelector("[data-score]");
+    const _highscore = document.querySelector("[data-high-score]");
+
+    if (score <= 9) _score.innerHTML = `0000${score}`;
+    else if (score >= 10 && score <= 99) _score.innerHTML = `000${score}`;
+    else if (score >= 100 && score <= 999) _score.innerHTML = `00${score}`;
+    else if (score >= 1000 && score <= 9999) _score.innerHTML = `0${score}`;
+    else if (score >= 10000 && score <= 99999) _score.innerHTML = `${score}`;
+    else if (score > 99999) _score.innerHTML = score;
+
+    // Override high score
+    const highscore = parseInt(document.querySelector("[data-high-score]").innerHTML);
+    if (score > highscore) {
+        if (score <= 9) _score.innerHTML = `0000${score}`;
+        else if (score >= 10 && score <= 99) _highscore.innerHTML = `000${score}`;
+        else if (score >= 100 && score <= 999) _highscore.innerHTML = `00${score}`;
+        else if (score >= 1000 && score <= 9999) _highscore.innerHTML = `0${score}`;
+        else if (score >= 10000 && score <= 99999) _highscore.innerHTML = `${score}`;
+        else if (score > 99999) _highscore.innerHTML = score;
+    }
+
+    if (dino.isAlive)
         setTimeout(function () {
             requestAnimationFrame(runAnimation);
         }, 1000 / fps);
@@ -82,7 +113,6 @@ function jump() {
         setTimeout(function () {
             char.classList.remove("jump");
             dino.state = "running";
-            runAnimation();
         }, 500);
     }
 }
