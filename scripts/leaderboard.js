8c5a17b1386324539b2a4e6f22113cd0c4ee1a24
Suezhoo <suezhoo@outlook.com>
2022-01-20 05:40:11 +0100

    Update leaderboard.js

--- scripts/leaderboard.js
+++ scripts/leaderboard.js
@@ -4,15 +4,15 @@ export async function displayScores() {
     await fetch("https://suezhoo-dinosaur.herokuapp.com/scores", { method: "GET" })
         .then((res) => res.json())
         .then((data) => {
+            if (data.length == 0) return;
             // Sort high to low
             data.sort(function (a, b) {
                 return b.score - a.score;
             });
             const positions = document.getElementsByClassName("positions");
-            for (let i = 0; i < positions.length; i++) {
+            for (let i = 0; i < data.length; i++) {
                 if (i <= 4) positions[i].textContent = `${data[i].score} - ${data[i].name}`;
                 else positions[i].textContent = `${data[i].name} - ${data[i].score}`;
             }
-            console.log("test");
         });
 }
